4fd33d3216b5f62e1212c7c8665d66a026e41fab
macmccallum <masonamccallum@gmail.com>
2026-05-24 18:58:06 +0000

    bump to nixos-unstable + hm master (nvim 0.12)

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Portable CLI home-manager config (macOS + Linux).";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
