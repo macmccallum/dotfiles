7fe656047889ef4693f0aa45604b8c5b2ed7e1df
macmccallum <masonamccallum@gmail.com>
2026-05-24 12:40:01 -0700

    linux fix

--- hm/linux.nix
+++ hm/linux.nix
@@ -8,9 +8,11 @@ in
   imports = [ ./common.nix ];
 
   # Bash rc files dropped verbatim from the repo.
-  home.file.".bashrc".source       = "${repo}/bash/.bashrc";
-  home.file.".bash_profile".source = "${repo}/bash/.bash_profile";
-  home.file.".bash_config".source  = "${repo}/bash/.bash_config";
+  # force = true lets Home Manager overwrite any pre-existing files at these
+  # paths instead of aborting with a "would be clobbered" error.
+  home.file.".bashrc"       = { source = "${repo}/bash/.bashrc";       force = true; };
+  home.file.".bash_profile" = { source = "${repo}/bash/.bash_profile"; force = true; };
+  home.file.".bash_config"  = { source = "${repo}/bash/.bash_config";  force = true; };
 
   # Linux-only packages go here when we need them. Empty for now.
   # home.packages = with pkgs; [ ... ];
